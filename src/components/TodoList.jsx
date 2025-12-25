6bc28c0e342697732f5850d687620b5e4d8a79ee
bravovo <bogdan.nyschyi@gmail.com>
2025-12-25 19:46:03 +0200

    added multiple categories for each todo

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -9,23 +9,35 @@ function TodoList({ todos, onDelete }) {
 
   return (
     <ul className="todo-list">
-      {todos.map(todo => (
-        <li key={todo.id} className="todo-item">
-          <div className="todo-content">
-            <span className="todo-text">{todo.text}</span>
-            <span className={`category-badge category-${todo.category || 'medium'}`}>
-              {todo.category || 'medium'}
-            </span>
-          </div>
-          <button
-            onClick={() => onDelete(todo.id)}
-            className="delete-button"
-            aria-label="Delete todo"
-          >
-            Delete
-          </button>
-        </li>
-      ))}
+      {todos.map(todo => {
+        // Support both old format (category) and new format (categories)
+        const categories = todo.categories || (todo.category ? [todo.category] : ['medium'])
+        
+        return (
+          <li key={todo.id} className="todo-item">
+            <div className="todo-content">
+              <span className="todo-text">{todo.text}</span>
+              <div className="category-badges">
+                {categories.map((category, index) => (
+                  <span 
+                    key={index} 
+                    className={`category-badge category-${category}`}
+                  >
+                    {category.replace('-', ' ')}
+                  </span>
+                ))}
+              </div>
+            </div>
+            <button
+              onClick={() => onDelete(todo.id)}
+              className="delete-button"
+              aria-label="Delete todo"
+            >
+              Delete
+            </button>
+          </li>
+        )
+      })}
     </ul>
   )
 }
